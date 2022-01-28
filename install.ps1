7646306b8e1d95075edd6bffea7dbca49df84cfa
Robbert Verbruggen <verbruggenrf@outlook.com>
2022-01-28 21:03:48 +0100

    feat: PowerShell with Oh My Posh profile

---
+++ install.ps1
@@ -0,0 +1,2 @@
+Copy-Item -Path "rfverbruggen.omp.json" -Destination $env:userprofile
+Copy-Item -Path "profile.ps1" -Destination "$($env:userprofile)\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
